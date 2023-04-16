7b5f0e2c21ef55587d0ad7ea63cb6eaebdaf4fb8
Sam Itani <itanio@gmail.com>
2023-04-16 11:13:01 +0400

    Set up shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import { useState } from "react";
+import { CalendarClock } from "lucide-react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const title = "Adventure Timer";
 
   return (
     <div className="App">
-      <header className="bg-slate-800">
-        <h1 className="text-4xl font-medium">{title}</h1>
+      <header className="bg-gray-200 flex gap-3 align-baseline text-left py-4 px-4">
+        <CalendarClock color="#303030" size={38} />
+        <h1 className="text-4xl font-medium text-gray-800">{title}</h1>
       </header>
-      <main>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test hot module reload.
-          </p>
-        </div>
-      </main>
-      <footer className="bg-slate-900">
+      <main>Hi there.</main>
+      <footer className="bg-gray-900">
         Built and maintained by <a href="https://www.samitani.net">Sam Itani</a>
         <div>
-          <a href="">Star this project on GitHub</a>
+          <a href="https://github.com/itanio/adventure-timer">
+            Star this project on GitHub
+          </a>
         </div>
       </footer>
     </div>
